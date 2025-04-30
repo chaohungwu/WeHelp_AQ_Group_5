a308c04b023ed6e8d597f094191ce42dd802a5b8
chaohungwu <asp5432101@gmail.com>
2025-05-01 05:39:14 +0800

    chart checkbox build and add some icon to webside

--- static/js/chart.js
+++ static/js/chart.js
@@ -3,6 +3,7 @@
 const api_key = "f2ef5c18-3af9-4678-8223-b3cd919591e0";
 let AQ_class = 'pm10subindex,pm25subindex,cosubindex,no2subindex,so2subindex,aqi' //等等用點擊按鈕的來加入變數
 let AQ_class_list = ['pm10subindex','pm25subindex',"cosubindex","no2subindex","so2subindex","aqi"];
+
 let currentSite = "中山";
 let currentSiteId = "12";
 
@@ -84,14 +85,13 @@ async function chartDomRender(){
 
 
 // 4. 繪製折線圖(資料label,空品資料)
-async function chartRender(currentSite){
-    let output_data = await AQ_data_format(AQ_class_list, currentSite);
-    
+async function chartRender(currentSiteId){
+    let output_data = await AQ_data_format(AQ_class_list, currentSiteId);
 
-    let Color = ["rgb(113, 59, 189)","rgb(59, 163, 189)","rgb(189, 133, 59)","rgb(59, 59, 189)","rgb(189, 59, 178)","rgb(10, 10, 10)"]
-    let backgroundColor = ["rgba(113, 59, 189, 0.1)","rgba(59, 163, 189, 0.1)","rgba(189, 133, 59, 0.1)","rgba(59, 59, 189, 0.1)","rgba(189, 59, 178, 0.1)","rgba(10, 10, 10, 0.1)"]
-
+    let Color = ["rgb(4, 150, 255)","rgb(255, 0, 0)","rgb(252, 143, 0)","rgb(59, 109, 189)","rgb(189, 59, 178)","rgb(113, 59, 189)"]
+    let backgroundColor = ["rgba(4, 150, 255, 0.1)","rgba(255, 0, 0, 0.1)","rgba(252, 143, 0, 0.1)","rgba(59, 109, 189, 0.1)","rgba(189, 59, 178, 0.1)","rgba(113, 59, 189, 0.1)"]
     let datasets = [];
+
     for(let n=0; n<output_data['AQ_class_list'].length; n++){
         let data = {
                     label: output_data['AQ_class_showText'][n], //數值名稱
@@ -106,145 +106,218 @@ async function chartRender(currentSite){
 
     const labels = output_data["date_list"].reverse();
 
+    let option1= {
+    responsive: true,
+    aspectRatio: 2,
+    scales: {
+            x: {
+                title: {
+                    display: true,
+                    text: 'Date',
+                    font: {
+                        size: 12
+                    }
+                },
+                
+                ticks: {
+                    font: {
+                        size: 10
+                    }
+                },
+                grid: {
+                    display: false
+                },
+            },
+
+            y: {
+                title: {
+                    display: true,
+                    text: 'ppb',
+                    font: {
+                        size: 12
+                    }
+                },
+                ticks: {
+                    font: {
+                        size: 10
+                    }
+                },
+                position: 'left',
+                beginAtZero: true,
+                // suggestedMin: 0,
+            },
+            
+            y1: {
+                title: {
+                    display: true,
+                    text: 'μg/m3',
+                    font: {
+                        size: 12
+                    }
+                },
+                ticks: {
+                    font: {
+                        size: 10
+                    }
+                },
+                grid: {
+                    display: false
+                },
+                position: 'right',
+                beginAtZero: true,
+                // suggestedMin: 0,
+            }
+        },
+        plugins: {
+            title: {
+                display: false,
+                text: '折線圖',
+                font: {
+                    size: 16
+                }
+            },
+            // colors: {
+            //     forceOverride: true
+            // },
+            legend: {
+                labels: {
+                    font: {
+                        size: 14
+                    }
+                }
+            },
+            tooltip: {
+                bodyFont: {
+                    size: 12
+                },
+                titleFont: {
+                    size: 12
+                },
+                xAlign: 'center',
+                yAlign: 'bottom',
+                titleAlign: 'center',
+            },
+        },
+        font:{
+            family: "'Noto Sans TC', sans-serif",
+        },
+        elements: {
+            
+            line: {
+                borderCapStyle: "round",
+                cubicInterpolationMode: 'monotone',
+                borderWidth: 2,
+            },
+            point: {
+                pointStyle: true,
+            }
+        },
+        interaction: {
+            intersect: false,
+        },
+        layout: {
+            padding: 10
+        }
+    }
+
     //如果圖表已經有存在的話
     if (myChart) {
         myChart.data.labels = labels;
         myChart.data.datasets = datasets;
         myChart.update(); // 更新圖表
     } else {
-        await chartDomRender() //新增放圖表的位置
+        chartDomRender() //新增放圖表的位置
         const config = {
             type: 'line',
             data: {
                 labels: labels,
                 datasets: datasets
             },
-            options: {
-                responsive: true,
-                aspectRatio: 2,
-                scales: {
-                        x: {
-                            title: {
-                                display: true,
-                                text: 'Date',
-                                font: {
-                                    size: 16
-                                }
-                            },
-                            
-                            ticks: {
-                                font: {
-                                    size: 14
-                                }
-                            },
-                            grid: {
-                                display: false
-                            },
-                        },
-
-                        y: {
-                            title: {
-                                display: true,
-                                text: 'ppb',
-                                font: {
-                                    size: 16
-                                }
-                            },
-                            ticks: {
-                                font: {
-                                    size: 14
-                                }
-                            },
-                            position: 'left',
-                            beginAtZero: true,
-                            // suggestedMin: 0,
-                        },
-                        
-                        y1: {
-                            title: {
-                                display: true,
-                                text: 'μg/m3',
-                                font: {
-                                    size: 16
-                                }
-                            },
-                            ticks: {
-                                font: {
-                                    size: 14
-                                }
-                            },
-                            grid: {
-                                display: false
-                            },
-                            position: 'right',
-                            beginAtZero: true,
-                            // suggestedMin: 0,
-                        }
-                    },
-                    plugins: {
-                        title: {
-                            display: false,
-                            text: '折線圖',
-                            font: {
-                                size: 20
-                            }
-                        },
-                        // colors: {
-                        //     forceOverride: true
-                        // },
-                        legend: {
-                            labels: {
-                                font: {
-                                    size: 18
-                                }
-                            }
-                        },
-                        tooltip: {
-                            bodyFont: {
-                                size: 16
-                            },
-                            titleFont: {
-                                size: 16
-                            },
-                            xAlign: 'center',
-                            yAlign: 'bottom',
-                            titleAlign: 'center',
-                        },
-                    },
-                    font:{
-                        family: "'Noto Sans TC', sans-serif",
-                    },
-                    elements: {
-                        
-                        line: {
-                            borderCapStyle: "round",
-                            cubicInterpolationMode: 'monotone',
-                            borderWidth: 2,
-                        },
-                        point: {
-                            pointStyle: true,
-                        }
-                    },
-                    interaction: {
-                        intersect: false,
-                    },
-                    layout: {
-                        padding: 20
-                    }
-                }
+                options:option1
             };
 
         let canvas_dom = document.querySelector('#canvas_dom');
         myChart = new Chart(canvas_dom, config);
+            }
+
     }
-}
+
 
 export default chartRender;
 
 
-// radio
-// async function AQ_class_radio(){
-//     let radio_group = document.createElement("div")
-//     document.querySelector(".controller").appendChild(radio_group)
-// }
+// checkbox
+async function AQ_class_checkbox_render(){
+    let AQ_class_name = ['PM10','PM2.5',"CO","CO2","SO2","AQI"];
+
+    let checkbox_group = document.createElement("div");
+    checkbox_group.className='checkbox_group';
+    document.querySelector(".chart").appendChild(checkbox_group)
+
+    // AQ_class_list
+    for(let i=0; i < AQ_class_list.length; i++){
+        let checkbox = document.createElement("input")
+        checkbox.type='checkbox';
+        checkbox.className='AQ_class_select';
+        checkbox.id= `AQ_class_select_${i}`;
+        checkbox.value=AQ_class_list[i];
+        checkbox.name='AQ_class_select';
+        checkbox.checked = true;
+        document.querySelector(".checkbox_group").appendChild(checkbox);
+    
+        let checkbox_label = document.createElement("label");
+        checkbox_label.htmlFor = `AQ_class_select_${i}`;
+        checkbox_label.id = `checkbox_label_${i}`;
+        checkbox_label.className = "checkbox_label";
+        document.querySelector(".checkbox_group").appendChild(checkbox_label);
+    
+        let checkbox_span = document.createElement("spqn");
+        checkbox_span.htmlFor = `AQ_class_select_${i}`;
+        checkbox_span.className = "checkbox_span";
+        checkbox_span.textContent=`✅ ${AQ_class_name[i]}`
+        checkbox_span.classList.add('active')
+        document.querySelector(`#checkbox_label_${i}`).appendChild(checkbox_span);
+
+        checkbox.addEventListener('change', async() => {
+            await AQ_class_checkbox_update()
+            let checked_class_num = document.querySelectorAll('input[name="AQ_class_select"]:checked');
+
+            chartRender(currentSiteId)
+
+            if (checkbox.checked) {
+                checkbox_span.classList.add('active');
+                let click_name = checkbox_span.textContent
+                // checkbox_span.textContent=`✅ ${click_name}`
+                checkbox_span.textContent=checkbox_span.textContent.replace("❌","✅")
+
+            } else {
+                checkbox_span.classList.remove('active');
+                checkbox_span.textContent=checkbox_span.textContent.replace("✅","❌")
+            }
+            // 剩下一個
+            if(checked_class_num.length==1){
+                checked_class_num[0].disabled = true;
+            }else{
+                
+                checked_class_num.forEach((checked) => {
+                    checked.disabled = false;
+                });
+            }
+
+        });
+
+    }
+}
+
+function AQ_class_checkbox_update(){
+    let checked_value = [];
+    let checkboxes = document.querySelectorAll('input[name="AQ_class_select"]:checked');
+    checkboxes.forEach((checkbox) => {
+      checked_value.push(checkbox.value);
+    });
+    AQ_class_list = checked_value
+    AQ_class = checked_value.join(',');
+}
+
+AQ_class_checkbox_render()
 
